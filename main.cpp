5d61cfd9ea295587f9078571126ded34ab9f8214
joannalew <lewj@oregonstate.edu>
2017-04-20 18:22:25 -0700

    Add files via upload
    hw1

--- main.cpp
+++ main.cpp
@@ -1,127 +1,232 @@
-/***********
+/*********************************
+ 
  * Joanna Lew
- * 4/13/17
- * HW 2(a) : Quaternary Search
- ***********/
+ * OSU CS 325 - Spring 2017
+ * Homework 1
+ * Comparing experimental run times of merge sort and insertion sort
+ 
+**********************************/
 
 #include <iostream>
+#include <chrono>
+#include <random>
 #include <vector>
-using namespace std;
 
-int algo1(vector<int> vec){
+typedef std::chrono::high_resolution_clock Clock;
+
+/** Sorts a vector of integers in ascending order using Insertion Sort
+    @param vec is the vector to be sorted (changed by function) */
+void insertionSort(std::vector<int> &vec){
     size_t size = vec.size();
-    int sum = 0;
-    int max = 0;
-    int maxlength = 0;
-    int left = 0;
+    int temp = 0;
     
-    for (int i=0; i < size; i++){
-        for (int j=i+1; j < size; j++){
-            sum = 0;
-            for (int k=i; k <= j; k++){
-                sum += vec[k];
-            }
-            if (sum > max){
-                left = i;
-                maxlength = j-i+1;
-                max = sum;
-            }
+    // for every element after the first element
+    for (int i = 1; i < size; ++i){
+        // compare each element with previous element
+        // if element is smaller, swap with previous
+        for (int j = i; j > 0 && vec[j-1] > vec[j]; --j){
+            temp = vec[j];
+            vec[j] = vec[j-1];
+            vec[j-1] = temp;
         }
     }
-    cout << "SubVector: ";
-    for (int i=left; i < maxlength; i++){
-        cout << vec[i] << " ";
-    }
-    cout << endl;
-    cout << "Sum: " << max << endl;
-
-    return 0;
+    return;
 }
 
-
-int algo2(vector<int> vec){
-    size_t size = vec.size();
-    int sum = 0;
-    int max = 0;
-    int maxlength = 0;
-    int left = 0;
+/** Helper Function for mergeSort
+    Merges two subvectors in ascending order
+    @param left is a subvector
+    @param right is a subvector
+    @param result is the resulting merged vector (changed by function) */
+void merge(std::vector<int> left, std::vector<int> right, std::vector<int> &result){
+    size_t lsize = left.size();
+    size_t rsize = right.size();
+    int lindex = 0;
+    int rindex = 0;
+    result.clear();
     
-    for (int i=0; i < size; i++){
-        sum = 0;
-        for (int j=i+1; j < size; j++){
-            sum += vec[j];
-            if (sum > max){
-                left = i;
-                maxlength = j-i+1;
-                max = sum;
-            }
+    // While there are elements in both subvectors
+    // Compare first element of each subvector to see which is smaller
+    // Add smaller element to result vector
+    while (lindex < lsize && rindex < rsize){
+        if (left[lindex] <= right[rindex]){
+            result.push_back(left[lindex]);
+            ++lindex;
+        }
+        else{
+            result.push_back(right[rindex]);
+            ++rindex;
         }
     }
-    cout << "SubVector: ";
-    for (int i=left; i < maxlength; i++){
-        cout << vec[i] << " ";
-    }
-    cout << endl;
-    cout << "Sum: " << max << endl;
     
-    return 0;
-}
-
-
-int max(int a, int b){
-    return (a > b) ? a : b;
-}
-
-int max(int a, int b, int c){
-    return max(max(a, b), c);
-}
-
-int algo3Helper(vector<int> vec, int start, int end, int mid){
-    int sum = 0, leftSum = 0, rightSum = 0;
-    
-    for (int i = mid; i >= start; i--){
-        sum += vec[i];
-        if (sum > leftSum)
-            leftSum = sum;
+    // Put any leftover elements in subvector
+    // into result vector
+    while (lindex < lsize){
+        result.push_back(left[lindex]);
+        ++lindex;
     }
     
-    sum = 0;
-    for (int i = mid+1; i <= end; i++){
-        sum += vec[i];
-        if (sum > rightSum)
-            rightSum = sum;
+    while(rindex < rsize){
+        result.push_back(right[rindex]);
+        ++rindex;
     }
     
-    return leftSum + rightSum;
+    return;
 }
 
-int algo3(vector<int> vec, int start, int end){
-    int mid = (end + start)/2;
+/** Sorts a vector of integers in ascending order using Merge Sort
+    @param vec is the vector to be sorted (changed by function) */
+void mergeSort(std::vector<int> &vec){
+    size_t size = vec.size();
     
-    if (vec.size() == 1)
-        return vec[0];
+    // base case: a list of 0 or 1 elements is sorted by definition
+    if (size <= 1)
+        return;
     
-    return (max(algo3(vec, start, mid),
-                algo3(vec, mid+1, end),
-                algo3Helper(vec, start, end, mid)));
+    // recursive case:
+    // divide the vector into equal-sized subvectors
+    // consisting of the first half and second half of vector
+    std::vector<int> left;
+    std::vector<int> right;
     
+    for (int i=0; i < size; ++i){
+        if (i < size / 2)
+            left.push_back(vec[i]);
+        else
+            right.push_back(vec[i]);
+    }
+    
+    // recursively divide subvectors
+    // until they are trivially sorted
+    mergeSort(left);
+    mergeSort(right);
+    
+    // merge sublists in sorted order
+    merge(left, right, vec);
+    
+    return;
 }
 
 
-int algo4(vector<int> vec){
-    vector<int> v2;
+/** Merges two subvectors into one vector
+    @param left is a subvector to be merged
+    @param right is a subvector to be merged
+    @param res is the resulting vector (changed by function) */
+void mergeUnsorted(std::vector<int> left, std::vector<int> right, std::vector<int> &res){
+    size_t lsize = left.size();
+    size_t rsize = right.size();
+    res.clear();
     
+    for (int i=0; i < lsize; ++i)
+        res.push_back(left[i]);
+    for (int j=0; j < rsize; ++j)
+        res.push_back(right[j]);
     
-    
-    return 0;
+    return;
 }
 
+/** Generates worst case vector for Merge Sort
+    @param vec is the vector to be turned into worst case 
+        (changed by function) */
+void unsort(std::vector<int> &vec){
+    size_t size = vec.size();
+    
+    if (size <= 1)
+        return;
+    
+    if (size == 2){
+        int swap = vec[0];
+        vec[0] = vec[1];
+        vec[1] = swap;
+        return;
+    }
+    
+    std::vector<int> left;
+    std::vector<int> right;
+    
+    for (int i=0; i < size; i+=2)
+        left.push_back(vec[i]);
+        
+    for (int i=1; i < size; i+=2)
+        right.push_back(vec[i]);
 
+    
+    unsort(left);
+    unsort(right);
+    mergeUnsorted(left, right, vec);
+    
+    return;
+}
 
-// testing
+/********************************************
+        MAIN
+ ********************************************/
+
 int main() {
-    vector<int> testVec = {31, -41, 59, 26, -53, 58, 97, -93, -23, 84};
-    algo2(testVec);
+    int numEle = 0;
+    std::vector<int> vlist;
+    
+    // get the number of elements from user
+    while (numEle < 1){
+        std::cout << "Please enter the number of elements in the vector: ";
+        std::cin >> numEle;
+    }
+    
+    // set the seed for random numbers in [0, vector size]
+    // 'rd' for non-deterministic 32-bit seed
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> dist(0, numEle);
+    
+    // average case:
+    // fill the vector with random numbers from [0, vector size]
+    
+    for (int i=0; i < numEle; ++i)
+        vlist.push_back(dist(mt));
+    
+    
+    // best case:
+    // fill the vector with numbers in ascending order
+    /*
+    for (int i=0; i < numEle; ++i)
+        vlist.push_back(i);
+    */
+    
+    // insertion sort worst case:
+    // fill the vector with numbers in descending order
+    /*
+    for (int i=numEle; i > 0; --i)
+        vlist.push_back(i);
+    */
+    
+    // merge sort worst case:
+    // make it so each subvector has the greatest # of comparisons/swaps
+    /*
+    for (int i=0; i < numEle; ++i)
+        vlist.push_back(i);
+    unsort(vlist);
+    */
+    
+    // perform sort
+    auto t1 = Clock::now();
+    //insertionSort(vlist);
+    mergeSort(vlist);
+    auto t2 = Clock::now();
+    
+    // get the time it took to perform sort
+    std::cout << "Delta t2-t1: "
+    << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()
+    << " nanoseconds" << std::endl;
+    std::cout << std::endl;
+    
+    
+    // Check if in sorted order
+    /*
+    for (auto x : vlist)
+        std::cout << x << " ";
+    std::cout << std::endl;
+    */
+    
     
     return 0;
 }
