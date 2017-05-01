08d84ac2c851127255398b466e4a8666a1ff7d01
olivasd <olivasd@oregonstate.edu>
2017-05-01 11:29:16 -0700

    updated timeChange

--- timeChange.cpp
+++ timeChange.cpp
@@ -3,167 +3,232 @@
 * CS325 project 2 algorithm run time comparison
 *********************************/
 #include "functions.hpp"
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <stdlib.h>
-#include <time.h>
+
 using namespace std;
 
+void runSlow(vector<int> V, ofstream &fout);
+void runGreedy(vector<int> V, ofstream &fout);
+void runDP(vector<int> V, ofstream &fout);
+void timeSlow( vector<vector<int>> Vectors, ofstream &fout);
+void timeGreedy( vector<vector<int>> Vectors, ofstream &fout);
+void timeDP( vector<vector<int>> Vectors, ofstream &fout);
+
 int main() {
-	double timer = 0;        //accumulates time for three runs of each algorithm
-	double avg_time = 0;     //holds average of three timing runs 
-	int a = 0;					//represents total amount trying to make change for
-	vector<int> denominations = [1, 5, 10, 25];
-	vector<int> *dnm = &denominations;
-	vector<int> change;      //holds correct change for each coin
-	
-	ofstream outputFile;
 
-	// Open the output file
-	outputFile.open("output.txt");
-
-
-
+	vector<int> V1 {1, 2, 12, 24, 48, 60};
+	vector<int> V2 {1, 5, 10, 25, 50};
+	vector<int> V3 {1, 6, 13, 37, 150};
+	vector<vector<int>> Vectors {V1, V2, V3};
+	vector<int> A;
+	ofstream fout;
+	fout.open("changeSlowAmount.txt");
 	//ALGORITHM #1: changeslow
-
-	//"a" variable may need to start at different value for different algorithms  
-	a = 50;
-	outputFile << "changeslow\n";
-	outputFile << "A, seconds\n";
-	
-	//test 10 different sizes of "a"
-	for (int i = 0; i < 10; ++i) {
-		cout << "round " << i+1 << "\n";
-		// find min coins using algorithm #1 and calculate run time
-		// for increased accuracy, run three times and calculate average run time
-		for (int k = 0; k < 3; ++k) {
-			clock_t t1, t2;
-			t1 = clock();
-			change = changeslow(dnm, a);
-			t2 = clock();
-			double diff((double)t2 - (double)t1);
-			timer += diff/CLOCKS_PER_SEC;
-			
-		}
-		avg_time = timer / 3.0;
-		
-		//display result
-		cout << "amount of change needed at each denomination:\n";
-		int minCoin = 0;
-		for (int i = 0; i < change.length; i++) {
-			cout << change.at(i) << " \n";
-			minCoin += change.at(i);
-		}
-		cout << "\nminimum number of coins needed: " << minCoin << endl;
-
-		//display time result
-		cout << "For A = " << a << ", algorithm #1 took " << avg_time << " seconds" << endl;
-		outputFile <<  a << ", " << avg_time << endl;
-
-		//use whatever pattern to update "a"
-		//default set to doubling "a" each iteration
-		a *= 2;
-		
-		//clear timer
-		timer = 0;
+	fout << "changeslow\n" << "A, Coins\n";
+	for (int i = 0; i < Vectors.size(); i++) {
+		runSlow(Vectors.at(i), fout);
+		fout << "\n";
 	}
-
-	//clear change vector
-	change.clear();
-	
+	fout.close();
+	fout.open("changegreedyAmount.txt");
 	//ALGORITHM #2: changegreedy
-
-	//"a" variable may need to start at different value for different algorithms  
-	a = 50;
-	outputFile << "changegreedy\n";
-	outputFile << "A, seconds\n";
-	
-	//test 10 different sizes of "a"
-	for (int i = 0; i < 10; ++i) {
-		cout << "round " << i+1 << "\n";
-		// find min coins using algorithm #2 and calculate run time
-		// for increased accuracy, run three times and calculate average run time
-		for (int k = 0; k < 3; ++k) {
-			clock_t t1, t2;
-			t1 = clock();
-			change = changegreedy(dnm, a);
-			t2 = clock();
-			double diff((double)t2 - (double)t1);
-			timer += diff/CLOCKS_PER_SEC;
-			
-		}
-		avg_time = timer / 3.0;
-		
-		//display result
-		cout << "amount of change needed at each denomination:\n";
-		int minCoin = 0;
-		for (int i = 0; i < change.length; i++) {
-			cout << change.at(i) << " \n";
-			minCoin += change.at(i);
-		}
-		cout << "\nminimum number of coins needed: " << minCoin << endl;
-
-		//display time result
-		cout << "For A = " << a << ", algorithm #2 took " << avg_time << " seconds" << endl;
-		outputFile <<  a << ", " << avg_time << endl;
-
-		//use whatever pattern to update "a"
-		//default set to doubling "a" each iteration
-		a *= 2;
-		
-		//clear timer
-		timer = 0;
+	fout << "changegreedy\n" << "A, Coins\n";
+	for (int i = 0; i < Vectors.size(); i++) {
+		runGreedy(Vectors.at(i), fout);
+		fout << "\n";
 	}
-
-	//clear change vector
-	change.clear();
-	
-	
-	//ALGORITHM #3: changedpp
-
-	//"a" variable may need to start at different value for different algorithms  
-	a = 50;
-	outputFile << "changedpp\n";
-	outputFile << "A, seconds\n";
-	
-	//test 10 different sizes of "a"
-	for (int i = 0; i < 10; ++i) {
-		cout << "round " << i+1 << "\n";
-		// find min coins using algorithm #3 and calculate run time
-		// for increased accuracy, run three times and calculate average run time
-		for (int k = 0; k < 3; ++k) {
-			clock_t t1, t2;
-			t1 = clock();
-			change = changedpp(dnm, a);
-			t2 = clock();
-			double diff((double)t2 - (double)t1);
-			timer += diff/CLOCKS_PER_SEC;
-			
-		}
-		avg_time = timer / 3.0;
-		
-		//display result
-		cout << "amount of change needed at each denomination:\n";
-		int minCoin = 0;
-		for (int i = 0; i < change.length; i++) {
-			cout << change.at(i) << " \n";
-			minCoin += change.at(i);
-		}
-		cout << "\nminimum number of coins needed: " << minCoin << endl;
-
-		//display time result
-		cout << "For A = " << a << ", algorithm #3 took " << avg_time << " seconds" << endl;
-		outputFile <<  a << ", " << avg_time << endl;
-
-		//use whatever pattern to update "a"
-		//default set to doubling "a" each iteration
-		a *= 2;
-		
-		//clear timer
-		timer = 0;
+	fout.close();
+	fout.open("chagedpAmount.txt");
+	//ALGORITHM #3: changedp
+	fout << "changedp\n" << "A, Coins\n";
+	for (int i = 0; i < Vectors.size(); i++) {
+		runDP(Vectors.at(i), fout);
+		fout << "\n";
 	}
+	fout.close();
 
-	
+	fout.open("changeSlowTime.txt");
+	//ALGORITHM #1: changeslow timing
+	fout << "changeslow\n" << "A, Coins\n";
+	timeSlow(Vectors, fout);
+	fout << "\n";
+	fout.close();
+
+	fout.open("changegreedyTime.txt");
+	//ALGORITHM #2: changegreedy timing
+	fout << "changegreedy\n" << "A, Coins\n";
+	timeGreedy(Vectors, fout);
+	fout << "\n";
+	fout.close();
+
+	fout.open("chagedpTime.txt");
+	//ALGORITHM #3: changedp timing
+	fout << "changedp\n" << "A, Coins\n";
+	timeDP(Vectors, fout);
+	fout << "\n";
+	fout.close();
+
 	return 0;
 }
+
+void runSlow(vector<int> V, ofstream &fout)
+{
+	int size = 45;
+	vector<int> A {};
+	vector<int> change(V.size(), 0);
+	for (int i = 1; i <= size; i++)
+	{
+		A.push_back(i);
+	}
+	fout << "[";
+	for (int i = 0; i < V.size(); i++)
+	{
+		fout << V.at(i);
+		if(i == V.size() -1 )
+			break;
+		else fout << ", ";
+	}
+	fout << "]\n";
+	for (int i = 0; i < A.size(); i++)
+	{
+		int sum = 0;
+		change = changeslow(V, A.at(i));
+
+		for(int j = 0; j < change.size(); j++)
+			sum += change.at(j);
+
+		fout << A.at(i) << " , " << sum << " \n";
+	}
+}
+
+void runGreedy(vector<int> V, ofstream &fout)
+{
+	int start = 2000;
+	int size = 2200;
+	vector<int> A {};
+	vector<int> change(V.size(), 0);
+	for (int i = 1; i <= size; i++)
+	{
+		A.push_back(i);
+	}
+	fout << "[";
+	for (int i = 0; i < A.size(); i++)
+	{
+		fout << V.at(i);
+		if(i == V.size() -1 )
+			break;
+		else fout << ", ";
+	}
+	fout << "]\n";
+	for (int i = start; i < A.size(); i++)
+	{
+		int sum = 0;
+		change = changegreedy(V, A.at(i));
+		for(int j = 0; j < change.size(); j++)
+			sum += change.at(j);
+
+		fout << A.at(i) << " , " << sum << " \n";
+	}
+}
+
+void runDP(vector<int> V, ofstream &fout)
+{
+	int start = 2000;
+	int size = 2200;
+	vector<int> A {};
+	vector<int> change(V.size(), 0);
+	for (int i = 1; i <= size; i++)
+	{
+		A.push_back(i);
+	}
+	fout << "[";
+	for (int i = 0; i < A.size(); i++)
+	{
+		fout << V.at(i);
+		if(i == V.size() -1 )
+			break;
+		else fout << ", ";
+	}
+	fout << "]\n";
+	for (int i = start; i < A.size(); i++)
+	{
+		int sum = 0;
+		change = changedp(V, A.at(i));
+
+		for(int j = 0; j < change.size(); j++)
+			sum += change.at(j);
+
+		fout << A.at(i) << " , " << sum << " \n";
+	}
+}
+
+void timeSlow( vector<vector<int>> Vectors, ofstream &fout)
+{
+	int size = 0;
+	double time = 0;
+	double avgTime = 0;
+	for(int k = 0; k < 10; k++) {
+		fout << "round " << k+1 << "\n";
+		for (int i = 0; i < Vectors.size(); i++) {
+			vector<int> change(Vectors.at(i).size(), 0);
+			clock_t t1, t2;
+			t1 = clock();
+			change = changeslow(Vectors.at(i), size);
+			t2 = clock();
+			double diff = ((double) t2 - (double) t1);
+			time += diff / CLOCKS_PER_SEC;
+			change.clear();
+		}
+		avgTime = time / 3.0;
+		fout << size << ", " << avgTime << "\n";
+		time = 0;
+		size += 5;
+	}
+}
+
+void timeGreedy( vector<vector<int>> Vectors, ofstream &fout)
+{
+	int size = 0;
+	double time = 0;
+	double avgTime = 0;
+	for(int k = 0; k < 10; k++) {
+		fout << "round " << k+1 << "\n";
+		for (int i = 0; i < Vectors.size(); i++) {
+			vector<int> change(Vectors.at(i).size(), 0);
+			clock_t t1, t2;
+			t1 = clock();
+			change = changegreedy(Vectors.at(i), size);
+			t2 = clock();
+			double diff = ((double) t2 - (double) t1);
+			time += diff / CLOCKS_PER_SEC;
+			change.clear();
+		}
+		avgTime = time / 3.0;
+		fout << size << ", " << avgTime << "\n";
+		time = 0;
+		size += 20000000;
+	}
+}
+
+void timeDP( vector<vector<int>> Vectors, ofstream &fout) {
+	int size = 0;
+	double time = 0;
+	double avgTime = 0;
+	for (int k = 0; k < 10; k++) {
+		fout << "round " << k + 1 << "\n";
+		for (int i = 0; i < Vectors.size(); i++) {
+			vector<int> change(Vectors.at(i).size(), 0);
+			clock_t t1, t2;
+			t1 = clock();
+			change = changedp(Vectors.at(i), size);
+			t2 = clock();
+			double diff = ((double) t2 - (double) t1);
+			time += diff / CLOCKS_PER_SEC;
+			change.clear();
+		}
+		avgTime = time / 3.0;
+		fout << size << ", " << avgTime << "\n";
+		time = 0;
+		size += 100000;
+	}
+}
+
